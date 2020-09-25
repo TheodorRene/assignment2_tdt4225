2640924a58acf1ecc412609df5ca8f3577190bd4
theodorc <theodor.rene@gmail.com>
2020-09-25 18:44:57 +0200

    Add function to add trackpoints to all users

--- assignment.py
+++ assignment.py
@@ -26,7 +26,6 @@ class Assignment:
         self.cursor = self.connection.cursor
         self.fs_helper = FileTraversal()
 
-
     def initate_database(self):
         """
         Initates the schema
@@ -46,6 +45,7 @@ class Assignment:
                 end_date_time DATETIME,
                 CONSTRAINT FK_user FOREIGN KEY (user_id)
                 REFERENCES user(id)
+                ON DELETE CASCADE
             )
             """
         create_trackpoint_table_q = """
@@ -54,10 +54,12 @@ class Assignment:
                 activity_id int,
                 lat DOUBLE,
                 lon DOUBLE,
+                altitude int,
                 date_days DOUBLE,
                 date_time DATETIME,
                 CONSTRAINT FK_activity FOREIGN KEY (activity_id)
                 REFERENCES activity(id)
+                ON DELETE CASCADE
             )
             """
         self.cursor.execute(create_user_table_q)
@@ -83,7 +85,8 @@ class Assignment:
         self.db_connection.commit()
 
     def truncate_table(self, table_name):
-        self.cursor.execute(f"TRUNCATE {table_name}")
+        """ Remove all data in table"""
+        self.cursor.execute(f"DELETE {table_name}")
         self.db_connection.commit()
         print(f"Truncated {table_name} table")
 
@@ -96,30 +99,113 @@ class Assignment:
         print(tabulate(rows, headers=self.cursor.column_names))
         return rows
 
-    def add_activity_for_user(self,user_id):
-       plts = self.fs_helper.get_all_plt_by_user_id(user_id)
-       for plt in plts:
+    def add_activity_for_user(self, user_id):
+        """ Add activity for a single user """
+        plts = self.fs_helper.get_all_plt_by_user_id(user_id)
+        for plt in plts:
             with plt.open() as f:
                 lines = f.readlines()
                 length_of_file = len(lines)
                 if length_of_file <= 2500:
                     transportation_mode = "NULL"
-                    start_date_time = self.fs_helper.parse_date_time_line(lines[6])
-                    end_date_time = self.fs_helper.parse_date_time_line(lines[length_of_file - 1])
+                    start_date_time = self.fs_helper.parse_date_time_line(
+                        lines[6])
+                    end_date_time = self.fs_helper.parse_date_time_line(
+                        lines[length_of_file - 1])
                     q = """ INSERT INTO activity (user_id,
                                                   transportation_mode,
                                                   start_date_time,
                                                   end_date_time)
                             VALUES ( '%s', '%s','%s', '%s')
                         """
-                    self.cursor.execute(q % (user_id, transportation_mode, start_date_time, end_date_time))
-       self.db_connection.commit()
+                    self.cursor.execute(
+                        q %
+                        (user_id,
+                         transportation_mode,
+                         start_date_time,
+                         end_date_time))
+        self.db_connection.commit()
 
+    def add_transportation_mode(self, user_id):
+        """Add transportation_mode to existing activites for one user """
+        data = self.fs_helper.get_start_time_and_label(user_id)
+        if data == [()]:
+            return
 
+        for records in data:
+            q = """ UPDATE activity
+                    SET transportation_mode='%s'
+                    WHERE user_id='%s' AND
+                          start_date_time='%s'
+                """
+            query = q % (records[1], user_id, records[0])
+            self.cursor.execute(query)
 
+        self.db_connection.commit()
 
+    def add_transportation_mode_all(self):
+        """ Add transportation_mode to all users """
+        users_ids = self.fs_helper.get_all_ids()
+        for user_id in users_ids:
+            self.add_transportation_mode(user_id)
 
+    def add_activity_for_all(self):
+        """ Add activity for all users"""
+        users_ids = self.fs_helper.get_all_ids()
+        for user_id in users_ids:
+            self.add_activity_for_user(user_id)
 
+    def add_activity(self):
+        """ Add activity and transportation_mode for all users"""
+        self.add_activity_for_all()
+        self.add_transportation_mode_all()
+
+    def add_trackpoints(self):
+        """ Add trackpoints for all users"""
+        users_ids = self.fs_helper.get_all_ids()
+        for user_id in users_ids:
+            self.add_trackpoint_by_user_id(user_id)
+
+    def add_trackpoint_by_user_id(self, user_id):
+        """ Add trackpoints for a user """
+        print("user_id", user_id) # TODO make progress bar
+        plts = self.fs_helper.get_all_plt_by_user_id(user_id)
+        for plt in plts:
+            date_id = self.fs_helper.filename_to_timestamp(plt)
+            with plt.open() as f:
+                lines = f.readlines()
+                length_of_file = len(lines)
+                if length_of_file <= 2500:
+                    activity_id = self.get_activity_id_by_date(
+                        user_id, date_id)
+                    q = """ INSERT INTO trackpoint (activity_id,
+                                                  lat,
+                                                  lon,
+                                                  altitude,
+                                                  date_days,
+                                                  date_time
+                                                  ) VALUES
+                        """
+                    for line in lines[6:]:
+                        lat, lon, _, altitude, date_days, date, time = line.split(
+                            ',')
+                        date_time = date + " " + time.rstrip()
+                        values = f"({activity_id}, {lat}, {lon}, {altitude}, {date_days}, '{date_time}'),"
+                        q += values
+                    # print(q[:-1])
+                    self.cursor.execute(q[:-1])
+
+        self.db_connection.commit()
+
+    def get_activity_id_by_date(self, user_id, date_id):
+        """ Get activity_id by date and user_id """
+        q = f"SELECT id FROM activity WHERE user_id='{user_id}' AND start_date_time='{date_id}'"
+        self.cursor.execute(q)
+        try:
+            activit_id = self.cursor.fetchone()[0]
+            return activit_id
+        except Exception as e:
+            print(q + "\n")
 
 
 def main():
@@ -129,10 +215,13 @@ def main():
     program = None
     try:
         program = Assignment()
-        #program.initate_database()
-        #program.add_users()
-        program.add_activity_for_user('000')
-        program.fetch_data('activity')
+        program.initate_database()
+      #  program.add_users()
+      #  program.add_activity()
+        program.add_trackpoints()
+        # program.fetch_data('activity')
+        # program.fetch_data('user')
+        program.fetch_data('trackpoint')
         program.show_tables()
     except Exception as e:
         print("ERROR: Failed to use database:", e)
--- traverse.py
+++ traverse.py
@@ -38,16 +38,8 @@ class FileTraversal:
         p = Path(self.ROOT) / user_id
         return len([x for x in p.iterdir()]) == 2
 
-    def get_label_for_activity(self, user_id, start_date_time):
-        if not self.has_labels(user_id):
-            return "NULL"
-        labels_txt = Path(self.ROOT) / user_id / 'labels.txt'
-        with labels_txt.open() as f:
-            for line in f.readlines()[1:]:
-                start_date = self.parse_date_time(f.split("\t")[0])
-                label = f.split("\t")[-1].rstrip()
-
     def get_start_time_and_label(self, user_id):
+        """ returns tuple of start_time and corrensponding label"""
         if not self.has_labels(user_id):
             return [()]
         labels_txt = Path(self.ROOT) / user_id / 'labels.txt'
@@ -59,28 +51,42 @@ class FileTraversal:
                 start_time_and_label.append((start_date, label))
         return start_time_and_label
 
-
     def parse_date_time_line(self, timestamp_line):
         """ Takes a line from an .plt file and returns the timestamp"""
-        return timestamp_line.split(',')[-2] + " " + timestamp_line.split(",")[-1]
+        return timestamp_line.split(
+            ',')[-2] + " " + timestamp_line.split(",")[-1]
 
     def parse_date_time(self, timestamp):
         """ takes in timestamp from labels.txt and converts to DB format """
-        return timestamp.split(' ')[0].replace('/','-') + " " + timestamp.split(" ")[1]
+        return timestamp.split(' ')[0].replace(
+            '/', '-') + " " + timestamp.split(" ")[1]
 
     def datetime_to_filename(self, timestamp):
         """ this will probably not be used, but converts DB timestamp to filename"""
-        return timestamp.replace(" ","").replace("-","").replace(":","") + ".plt"
+        return timestamp.replace(
+            " ",
+            "").replace(
+            "-",
+            "").replace(
+            ":",
+            "") + ".plt"
+
+    def filename_to_timestamp(self, filename):
+        with filename.open() as f:
+            lines = f.readlines()
+            return self.parse_date_time_line(lines[6])
+
 
 def main():
     """
     Main function
     """
     file_object = FileTraversal()
-    #print(file_object.path_to_users())
-    #print(file_object.get_all_plt_by_user_id("000"))
-    s = file_object.get_start_time_and_label("010")[0][0]
-    print(file_object.datetime_to_filename(s))
+    # print(file_object.path_to_users())
+    # print(file_object.get_all_plt_by_user_id("000"))
+    s = file_object.filename_to_timestamp("20090611221204.plt")
+    print(s)
+
 
 if __name__ == '__main__':
     main()
