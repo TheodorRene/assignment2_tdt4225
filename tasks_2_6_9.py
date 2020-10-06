595170b950b2d8281e3cd0c7c9cc941f1a1fc9ad
Eivind <eivind.kop@gmail.com>
2020-10-06 10:11:56 +0200

    Add task 9
    The initial time stamp is hard coded, but it makes the dream work.
    The query results in 11 000 invalid activities, so I'm not entirely sure
    if the query is correct (:

--- tasks_2_6_9.py
+++ tasks_2_6_9.py
@@ -1,10 +1,8 @@
 from sys import argv
 from tabulate import tabulate
 from DbConnector import DbConnector
+
 class Tasks:
-    """
-    Main class
-    """
 
     def __init__(self):
         """ Sets up database connection """
@@ -23,8 +21,8 @@ class Tasks:
         def count_table(table_name):
             """ Return number of instances in a given table"""
 
-            q_count_table = f"SELECT count(id) FROM {table_name}"
-            self.cursor.execute(q_count_table)
+            query_count_table = f"SELECT count(id) FROM {table_name}"
+            self.cursor.execute(query_count_table)
             rows = self.cursor.fetchall()
             return rows[0][0]
 
@@ -61,12 +59,38 @@ class Tasks:
         rows = self.cursor.fetchall()
         print(tabulate(rows, headers=self.cursor.column_names))
 
+    def task9(self):
+        """ Find all users who have invalid activities, and the number of invalid activities per user. """
+
+        lag_timestamp_table = "SELECT activity_id, @timestamp lag_timestamp, @timestamp:=date_time curr_timestamp " \
+                              "FROM trackpoint " \
+                              "ORDER BY activity_id, date_time" \
+
+        invalid_activity_table = "SELECT DISTINCT activity_id " \
+                                 f"FROM ({lag_timestamp_table}) AS lag_timestamp_table " \
+                                 "WHERE TIMESTAMPDIFF(MINUTE, lag_timestamp, curr_timestamp) > 5 " \
+
+        query = "SELECT user_id, COUNT(activity_id) as invalid_activity_count " \
+                f"FROM activity INNER JOIN ({invalid_activity_table}) AS invalid_activity_table " \
+                "ON activity.id = invalid_activity_table.activity_id " \
+                "GROUP BY user_id"
+
+        timestamp_setup = "SET @timestamp='2009-05-30 03:44:45';"
+
+        self.cursor.execute(timestamp_setup)
+        self.cursor.execute(query)
+        print(self.cursor.statement)
+        rows = self.cursor.fetchall()
+        print(tabulate(rows, headers=self.cursor.column_names))
+
+
 
 def main():
     program = Tasks()
     program.task2()
     program.task6a()
     program.task6b()
+    program.task9()
 
 if __name__ == '__main__':
     main()
