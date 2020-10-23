c83195965e918678b5f49ef11dc6f9e1cab09e4b
Ingrid Johansen <ingridmj95@gmail.com>
2020-10-23 11:35:18 +0200

    Add tasks to all_tasks file

--- assignment3/all_tasks.py
+++ assignment3/all_tasks.py
@@ -1,3 +1,5 @@
+import math
+import time
 from sys import argv
 from DbConnector import DbConnector
 from pprint import pprint
@@ -28,6 +30,11 @@ class Tasks:
         count_collection("activity")
         count_collection("trackpoint")
 
+    def task_2(self):
+        """ Find the average activities per user. """
+
+        pprint(self.db["activity"].count() / self.db["user"].count())
+
     def task_4(self):
         """Find all users who have taken a taxi"""
         collection = self.db["activity"]
@@ -53,6 +60,67 @@ class Tasks:
                         }}
         ]
         pprint(list(collection.aggregate(pipeline)))
+
+    def task_6a(self):
+        """ Find the year with the most recorded activities. """
+
+        collection = self.db["activity"]
+        documents = collection.aggregate([
+            {
+                "$group": {
+                    "_id": {
+                        "year": {"$year": {"$toDate": "$start_date_time"}}
+                    },
+                    "activity_count": {"$sum": 1}
+                }
+            },
+            {
+                "$sort": {"activity_count": -1}
+            },
+            {
+                "$limit": 1
+            }
+        ]
+        )
+        for document in documents:
+            pprint(document)
+
+    def task_6b(self):
+        """ Is the year with the most activities the year with the most recorded hours? """
+
+        collection = self.db["activity"]
+        documents = collection.aggregate([
+            {
+                "$group": {
+                    "_id": {
+                        "year": {"$year": {"$toDate": "$start_date_time"}}
+                    },
+                    "minute_count": {
+                        "$sum": {
+                            "$divide": [
+                                {
+                                    "$subtract":
+                                        [
+                                            {"$toDate": "$end_date_time"},
+                                            {"$toDate": "$start_date_time"}
+                                        ]
+                                },
+                                60000
+                            ]
+                        }
+                    }
+                }
+            },
+            {
+                "$sort": {"minute_count": -1}
+            },
+            {
+                "$limit": 1
+            }
+        ]
+        )
+        for document in documents:
+            pprint(document)
 
     def task_8(self):
         """Find the top 20 users who have gained the most altitude meters"""
@@ -87,6 +155,60 @@ class Tasks:
         top_20.reverse()
         print("Top 20:")
         pprint(top_20)
+
+    def task_9(self):
+        """ Find all users who have invalid activities, and the number of invalid activities per user. """
+
+        def _minute_difference(from_date, to_date):
+            """ Calculates the difference in minutes between @param from_date and @param to_date. """
+            from_date_stamp = time.strptime(from_date, "%Y-%m-%d %H:%M:%S")
+            to_date_stamp = time.strptime(to_date, "%Y-%m-%d %H:%M:%S")
+
+            from_date_unix = time.mktime(from_date_stamp)
+            to_date_unix = time.mktime(to_date_stamp)
+
+            return int((to_date_unix - from_date_unix) / 60)
+
+        def _find_user_by_activity_id(db, activity_id):
+            """ Returns the user that the given @param activity_id belongs to. """
+
+            activities = db["activity"]
+            activity = activities.find_one({"_id": activity_id})
+            return activity["user_id"]
+
+        collection = self.db["trackpoint"]
+        documents = collection.aggregate(
+            [
+                {"$project": {"_id": 1, "date_time": 1, "activity_id": 1}},
+                {"$sort": {"activity_id": 1, "date_time": 1}},
+            ], allowDiskUse=True
+        )
+
+        invalid_activities = []
+        invalid_activity_count = {}
+        previous_document = None
+        for document in documents:
+            if not previous_document or (previous_document["activity_id"] != document["activity_id"]):
+                previous_document = document
+                continue
+
+            if _minute_difference(previous_document["date_time"], document["date_time"]) >= 5:
+                if document["activity_id"] not in invalid_activities:
+                    invalid_activities.append(document["activity_id"])
+
+            previous_document = document
+
+        for invalid_activity in invalid_activities:
+            invalid_user = _find_user_by_activity_id(self.db, invalid_activity)
+
+            if invalid_user in invalid_activity_count.keys():
+                invalid_activity_count[invalid_user] += 1
+            else:
+                invalid_activity_count[invalid_user] = 1
+
+        pprint(invalid_activity_count)
+
+
 
     def task_11(self):
         """Find all users who have registered transportation_mode and their most used transportation_mode."""
